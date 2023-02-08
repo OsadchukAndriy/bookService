9b7f1fc563a4292ac4844426c1102ce1a7611f4f
andrejosadcuk <Rudyst.1995@gmail.com>
2023-02-08 22:38:03 +0200

    !

--- src/main/java/ua/osadchuk/bookService/controllers/BookController.java
+++ src/main/java/ua/osadchuk/bookService/controllers/BookController.java
@@ -3,42 +3,64 @@ package ua.osadchuk.bookService.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ua.osadchuk.bookService.models.Book;
+import ua.osadchuk.bookService.models.Person;
 import ua.osadchuk.bookService.services.BooksService;
+import ua.osadchuk.bookService.services.PeopleService;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @Controller
-@RequestMapping("/books")
+@RequestMapping("/")
 public class BookController {
 
     private final BooksService booksService;
+    private final PeopleService peopleService;
 
-    public BookController(BooksService booksService) {
+    public BookController(BooksService booksService, PeopleService peopleService) {
         this.booksService = booksService;
+        this.peopleService = peopleService;
+    }
+    @GetMapping("books/{id}")
+    public String show(@PathVariable("id") int id, Model model,
+                       @ModelAttribute("person")Person person){
+        model.addAttribute("book", booksService.findOne(id));
+
+        Person bookOwner = booksService.getBookOwner(id);
+        if (bookOwner != null)
+            model.addAttribute("owner", bookOwner);
+        else
+            model.addAttribute("people", peopleService.findAll());
+
+        return "books/show";
+
     }
 
-    @GetMapping()
+    @GetMapping("/books")
     public String index(Model model){
         model.addAttribute("books", booksService.findAll());
         return "books/index";
     }
 
+    @GetMapping("books/new")
     public String newBook(@ModelAttribute("book") @Valid Book Book){
         return "books/new";
     }
 
-    @PostMapping
+    @PostMapping("people/books")
     public String create(@ModelAttribute("book") @Valid Book book,
     BindingResult bindingResult){
         if(bindingResult.hasErrors()){
             return "books/new";
         }
         booksService.cave(book);
+        return "redirect:/books";
+    }
+    @DeleteMapping("books/{id}")
+    public String delete(@PathVariable("id") int id){
+        booksService.delete(id);
         return "redirect:/books";
     }
 }
