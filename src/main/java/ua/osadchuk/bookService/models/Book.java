35f48d3cb90460842c014b735efb69b96adccb1f
Taras Osadchuk <tarasosadchuk.v@gmail.com>
2023-02-08 17:54:20 +0200

    Update models Person and Book

--- src/main/java/ua/osadchuk/bookService/models/Book.java
+++ src/main/java/ua/osadchuk/bookService/models/Book.java
@@ -1,4 +1,85 @@
 package ua.osadchuk.bookService.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Book")
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "author")
+    private String author;
+
+    @Column(name = "year_of_release")
+    private int yearOfRealise;
+
+    @ManyToOne
+    @JoinColumn(name = "person_id", referencedColumnName = "id")
+    private Person owner;
+
+    public Book() {
+    }
+
+    public Book(String name, String author, int yearOfRealise) {
+        this.name = name;
+        this.author = author;
+        this.yearOfRealise = yearOfRealise;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYearOfRealise() {
+        return yearOfRealise;
+    }
+
+    public void setYearOfRealise(int yearOfRealise) {
+        this.yearOfRealise = yearOfRealise;
+    }
+
+    public Person getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Person owner) {
+        this.owner = owner;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                ", yearOfRealise=" + yearOfRealise +
+                '}';
+    }
 }
--- src/main/java/ua/osadchuk/bookService/models/Person.java
+++ src/main/java/ua/osadchuk/bookService/models/Person.java
@@ -1,4 +1,59 @@
 package ua.osadchuk.bookService.models;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "Person")
 public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(mappedBy = "owner")
+    private List<Book> books;
+
+    public Person() {
+    }
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
