c439dfd0f87aedc1d03c9c3d7008c2264dce217a
andrejosadcuk <Rudyst.1995@gmail.com>
2023-02-08 18:46:48 +0200

    ok

--- src/main/java/ua/osadchuk/bookService/controllers/BookController.java
+++ src/main/java/ua/osadchuk/bookService/controllers/BookController.java
@@ -2,9 +2,15 @@ package ua.osadchuk.bookService.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ua.osadchuk.bookService.models.Book;
 import ua.osadchuk.bookService.services.BooksService;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/books")
@@ -20,5 +26,19 @@ public class BookController {
     public String index(Model model){
         model.addAttribute("books", booksService.findAll());
         return "books/index";
+    }
+
+    public String newBook(@ModelAttribute("book") @Valid Book Book){
+        return "books/new";
+    }
+
+    @PostMapping
+    public String create(@ModelAttribute("book") @Valid Book book,
+    BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return "books/new";
+        }
+        booksService.cave(book);
+        return "redirect:/books";
     }
 }
--- src/main/java/ua/osadchuk/bookService/services/PeopleService.java
+++ src/main/java/ua/osadchuk/bookService/services/PeopleService.java
@@ -1,4 +1,7 @@
 package ua.osadchuk.bookService.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class PeopleService {
 }
